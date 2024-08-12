06fe66ee9d0f2b1bb97698f5cf9f6fb14ab032b8
bvp1518 <bvp18061992@gmail.com>
2024-08-12 18:19:39 +0530

    till-233. Using async / await

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -8,15 +8,26 @@ import { useState } from "react";
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
  
-  useEffect(() => {
-    fetch("http://localhost:3000/places")
-    .then((response) => {      
-      return response.json();
-    })
-    .then ((resData) =>{
-      setAvailablePlaces(resData.places);
-    });
+useEffect(() => {
+async function fetchPlaces() {
+  const response = await fetch("http://localhost:3000/places");
+  const resData = await response.json();
+  setAvailablePlaces(resData.places);
+}
+fetchPlaces();
   }, []);
+
+
+
+  // useEffect(() => {
+  //   fetch("http://localhost:3000/places")
+  //   .then((response) => {      
+  //     return response.json();
+  //   })
+  //   .then ((resData) =>{
+  //     setAvailablePlaces(resData.places);
+  //   });
+  // }, []);
 
    
   return (
